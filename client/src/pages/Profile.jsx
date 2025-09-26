1328a3147cd013c36520a9242176387e65503184
skrsikop <skrsikop@gmail.com>
2025-09-26 14:27:14 +0300

    context

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import assets from "../assets/assets";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../../context/authContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const Profile = () => {
   const {authUser, updateProfile} = useContext(AuthContext)
